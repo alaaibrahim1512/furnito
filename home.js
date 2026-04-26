d985e624c0054be5fc1d61b67c5d9fff58789145
alaa <ialaa3942@gmail.com>
2026-04-26 23:15:30 +0300

    final update

--- home.js
+++ home.js
@@ -3,4 +3,13 @@ function gotoshop(){
     window.location.href= "shop.html";
     return false;
 }
+function goToProduct(id) {
+    // سيقوم بفتح صفحة المنتج وتمرير الـ ID في الرابط
+    window.location.href = "product.html?id=" + id;
+}
+
+function gotoshop(){
+    window.location.href = "shop.html";
+    
+}
 
